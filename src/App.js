07426458496cc7065352b109d65ac5f7ca350e09
Kenneth Lau <drcylau@hotmail.com>
2023-08-10 18:16:46 +0100

    correct typing mistake

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Feather bramch Edit <code>src/App.js</code> and save to reload.
+          Feather branch Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
