f8ce627f48e44683bff0786b35476da7c87e8450
Kenneth Lau <drcylau@hotmail.com>
2023-08-10 20:03:19 +0100

    change from feature branch text2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Feather branch Edit 1<code>src/App.js</code> and save to reload.
+          Feather branch Edit 2<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
